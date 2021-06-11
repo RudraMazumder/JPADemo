64a7555c0f585cadc9d9a64d343b878be5a43393
RUMAZUMD <RUMAZUMD@LBNL734795.mshome.net>
2021-06-11 13:54:47 +0200

    AOP - After and before

--- src/main/java/com/example/demo/CrossCuttingAspects.java
+++ src/main/java/com/example/demo/CrossCuttingAspects.java
@@ -1,20 +1,36 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.model.Book;
+
+/**
+* Author - RUMAZUMD
+* CreatedDate - 11 jun. 2021
+* 
+**/
 @Aspect
 @Configuration
-public class BeforeAspect {
-
+public class CrossCuttingAspects {
 	private Logger logger = LoggerFactory.getLogger(this.getClass()); 
 	
 	@Before("execution(* com.example.demo.service.*.*(..))")
 	public void before(JoinPoint joinPoint) {
 		logger.info("Intercepting {}",joinPoint);
 	}
+	@AfterReturning(value = "execution(* com.example.demo.service.*.*(..))",returning = "result" )
+	public void afterReturn(JoinPoint joinPoint, List<Book> result) {
+		int listSize=result.size();
+		logger.info("Intercepting after return {} with the list size {}",joinPoint, listSize);
+		
+	}
+
 }
